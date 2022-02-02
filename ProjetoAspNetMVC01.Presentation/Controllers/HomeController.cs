435f1f8f7d83468d9c2afac5d44d2c5e4f2c8c02
Palheiros <ricardopalheiros@gmail.com>
2022-02-02 12:01:29 -0300

    Primeira versão

---
+++ ProjetoAspNetMVC01.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoAspNetMVC01.Presentation.Controllers
+{
+    //método utilizado para criar a página
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoAspNetMVC01.Presentation/Controllers/TarefasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAspNetMVC01.Presentation.Models;
+
+namespace ProjetoAspNetMVC01.Presentation.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost] //Indica que o método é executado pelo botão SUBMIT do formulário
+        public IActionResult Cadastro(TarefasCadastroModel model)
+        {
+            return View();
+        }
+
+        public IActionResult Consulta()
+        {
+            return View();
+        }
+
+        public IActionResult Relatorio()
+        {
+            return View();
+        }
+    }
+}
